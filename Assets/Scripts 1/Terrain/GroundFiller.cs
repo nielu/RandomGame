239fc2754073e1f22b597db792b86da0d56691eb
jan nielek <jan.nielek@intel.com>
2016-02-23 16:20:32 +0100

    todo: ground

---
+++ Assets/Scripts 1/Terrain/GroundFiller.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundFiller : MonoBehaviour 
+{
+    public GameObject DirtBlock;
+    public int HowMuch;
+
+
+    private Vector2 lastPos;
+	// Use this for initialization
+	void Start () 
+    {
+        lastPos = transform.position;
+        SpawnGround();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (lastPos.x + 0.7f >= transform.position.x)
+        {
+            SpawnGround();
+            lastPos = transform.position;
+        }
+	}
+
+
+    void SpawnGround()
+    {
+        for (int i = 0; i < HowMuch; i++)
+        {
+            Instantiate(DirtBlock, lastPos + new Vector2(0, -1.4f), Quaternion.EulerAngles(0,0,0));
+        }
+    }
+}
