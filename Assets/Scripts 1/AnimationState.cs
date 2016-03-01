b0234b623bc6f738fc1af6899c7fa87b3c605a97
nielu <nielu92@gmail.com>
2016-03-01 22:22:52 +0100

    add side jump prefab
    TBD - player rotation!!!

--- Assets/Scripts 1/AnimationState.cs
+++ Assets/Scripts 1/AnimationState.cs
@@ -4,56 +4,61 @@ using System.Collections;
 public class AnimationState : MonoBehaviour
 {
 
-    private Animator anim;
-    private Rigidbody2D body;
+ private Animator anim;
+ private Rigidbody2D body;
 
-    private bool facingRight = true;
-    private string paramName = "state";
-    private float delta = 0.1f;
+ private bool facingRight = true;
+ private string paramName = "state";
+ private float delta = 0.1f;
 
-    enum state
-    {
-        stand = 0,
-        walk,
-        jump,
-        fall,
-        climb
-        
-    };
-	// Use this for initialization
-	void Start ()
-    {
-        anim = GetComponent<Animator>();
-        body = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        var vX = body.velocity.x;
-        var vY = body.velocity.y;
+ enum state
+ {
+  stand = 0,
+  walk,
+  jump,
+  fall,
+  climb
 
-        var newState = state.stand;
+ };
+ // Use this for initialization
+ void Start()
+ {
+  anim = GetComponent<Animator>();
+  body = GetComponent<Rigidbody2D>();
+ }
 
-        if ((vX < -delta && facingRight == true) || (vX > delta && facingRight == false))
-            flip();
+ // Update is called once per frame
+ void Update()
+ {
+  var vX = body.velocity.x;
+  var vY = body.velocity.y;
 
-        if (vY < -delta)
-            newState = state.fall;
-        else if (vY > delta)
-            newState = state.jump;
-        else if (vX > delta || vX < -delta)
-            newState = state.walk;
+  var newState = state.stand;
 
-        anim.SetInteger(paramName, (int)newState);
-            
-	}
+  var isUpright = Mathf.Abs(body.rotation) < delta;
 
-    void flip()
-    {
-        facingRight = !facingRight;
-        var scale = transform.localScale;
-        scale.x = scale.x * -1f;
-        transform.localScale = scale;
-    }
+  if ((vX < -delta && facingRight == true) || (vX > delta && facingRight == false))
+   flip();
+
+
+
+
+  if (vY < -delta && isUpright)
+   newState = state.fall;
+  else if (vY > delta && isUpright)
+   newState = state.jump;
+  else if (vX > delta || vX < -delta)
+   newState = state.walk;
+
+  anim.SetInteger(paramName, (int)newState);
+
+ }
+
+ void flip()
+ {
+  facingRight = !facingRight;
+  var scale = transform.localScale;
+  scale.x = scale.x * -1f;
+  transform.localScale = scale;
+ }
 }
