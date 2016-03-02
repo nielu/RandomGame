676df2de1daaeec5bf2a7e7fce3174208d714d87
jan nielek <jan.nielek@intel.com>
2016-03-02 16:24:44 +0100

    WIP on menu

---
+++ Assets/Scripts 1/Menu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+    public Canvas quitMenu;
+    public Button startNormal;
+    public Button startWierd;
+    public Button exit;
+
+
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
