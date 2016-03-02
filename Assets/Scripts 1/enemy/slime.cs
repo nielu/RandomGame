c3bd14ae285a2addab9c874906fec7a767c53a74
jan nielek <jan.nielek@intel.com>
2016-03-02 15:25:03 +0100

    add background

--- Assets/Scripts 1/enemy/slime.cs
+++ Assets/Scripts 1/enemy/slime.cs
@@ -1,32 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class slime : enemy 
+public class slime : enemy
 {
- protected override void Start()
- {
-  base.Start();
- }
+    protected override void Start()
+    {
+        base.Start();
+    }
 
- protected override void Update()
- {
-  base.Update();
+    protected override void Update()
+    {
+        base.Update();
 
-  if (Vector3.Distance(body.position, player.transform.position) < 15f)
-  {
-   transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
-  }
 
-  if (goRight && player.transform.position.x - transform.position.x < 0)
-   flip();
-  else if (!goRight && transform.position.x - player.transform.position.x < 0)
-   flip();
 
- }
+        if (goRight && !GetGround(0.5f, 0.75f, 0f))
+        {
+            goRight = false;
+            return;
+        }
+        if (!goRight && !GetGround(0.5f, -0.75f, 0f))
+        {
+            goRight = true;
+            return;
+        }
 
- protected override void OnRaycastHit2D(Collision coll)
- {
-  base.OnRaycastHit2D(coll);
- }
+        if (Vector3.Distance(body.position, player.transform.position) < 15f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+
+        if (goRight && player.transform.position.x - transform.position.x < 0)
+            flip();
+        else if (!goRight && transform.position.x - player.transform.position.x < 0)
+            flip();
+
+    }
+
+    protected override void OnCollisionEnter2D(Collision2D coll)
+    {
+
+        base.OnCollisionEnter2D(coll);
+    }
+
+    
 
 }
