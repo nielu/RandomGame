5a8d58100ffcde4083eb1ab0dd872f70be26cbbd
jan nielek <jan.nielek@intel.com>
2016-03-04 13:32:58 +0100

    Add ProceduralToolkit

--- Assets/Scripts 1/LevelGenerator.cs
+++ Assets/Scripts 1/LevelGenerator.cs
@@ -3,81 +3,94 @@ using System.Collections;
 
 public class LevelGenerator : MonoBehaviour
 {
- public int Seed;
- public int OctaveCount;
- public float Persistence;
+    public int Seed;
+    public int OctaveCount;
+    public float Persistence;
 
- public float MaxLevelLength;
- public float MinLevelLength;
+    public float MaxLevelLength;
+    public float MinLevelLength;
 
- public GameObject StartingTerrain;
- public GameObject EndingTerrain;
+    public GameObject StartingTerrain;
+    public GameObject EndingTerrain;
 
- public GameObject[] TerrainBlocks;
+    public GameObject[] TerrainBlocks;
 
 
 
- private Vector3 EndingPosition;
- private Vector3 ActualPosition;
+    private Vector3 EndingPosition;
+    private Vector3 ActualPosition;
 
- // Use this for initialization
- void Start()
- {
-  var xPos = 0.0f;
-  for (int i = 0; i < 100; i++)
-  {
-   float h = Mathf.PerlinNoise(Time.time + Random.Range(0f, 5f), 0f);
-   Instantiate(TerrainBlocks[0], new Vector3(xPos, h, 0), Quaternion.Euler(0, 0, 0));
-   xPos += 0.7f;
-  }
+    // Use this for initialization
+    void Start()
+    {
+        var xPos = 0.0f;
 
-  //    if (Seed == 0)
-  //    {
-  //        Seed = System.DateTime.Now.GetHashCode();
-  //    }
+        float[,] map = new float[16,16];
 
-  //    Random.seed = Seed;
+        for (int x = 0; x < 16; x++)
+            for (int y = 0; y < 16; y++)
+            {
+                map[x, y] = Mathf.PerlinNoise(x * 1.01f, y * 1.01f);
+                if (map[x, y] > .5f)
+                    Instantiate(TerrainBlocks[0], new Vector3(x * 0.7f, y * 0.7f, 0), Quaternion.Euler(0, 0, 0));
+            }
 
-  //    ActualPosition = new Vector3(0, 0, 0);
-  //    EndingPosition = new Vector3(Random.Range(MinLevelLength, MaxLevelLength), 0, 0);
 
-  //    SpawnTerrain(StartingTerrain);
 
-  //    while (ActualPosition.x < EndingPosition.x)
-  //    {
-  //        GameObject terrain = SelectTerrain();
-  //        SpawnTerrain(terrain);
-  //    }
+        //for (int i = 0; i < 100; i++)
+        //{
+        //    float h = Mathf.PerlinNoise(Time.time, 0f);
+        //    Instantiate(TerrainBlocks[0], new Vector3(xPos, h, 0), Quaternion.Euler(0, 0, 0));
+        //    xPos += 0.7f;
+        //}
 
-  //    SpawnTerrain(EndingTerrain);
+        //    if (Seed == 0)
+        //    {
+        //        Seed = System.DateTime.Now.GetHashCode();
+        //    }
 
- }
+        //    Random.seed = Seed;
 
- private GameObject SelectTerrain()
- {
-  var r = Random.Range(0, TerrainBlocks.Length);
-  return TerrainBlocks[r];
- }
+        //    ActualPosition = new Vector3(0, 0, 0);
+        //    EndingPosition = new Vector3(Random.Range(MinLevelLength, MaxLevelLength), 0, 0);
 
- // Update is called once per frame
- void Update()
- {
- }
+        //    SpawnTerrain(StartingTerrain);
 
- void SpawnTerrain(GameObject terrain)
- {
-  SpawnTerrain(terrain, ActualPosition);
- }
+        //    while (ActualPosition.x < EndingPosition.x)
+        //    {
+        //        GameObject terrain = SelectTerrain();
+        //        SpawnTerrain(terrain);
+        //    }
 
- void SpawnTerrain(GameObject terrain, Vector3 Position)
- {
-  Instantiate(terrain, Position, Quaternion.Euler(0, 0, 0));
-  ActualPosition = UpdatePosition(terrain, Position);
- }
+        //    SpawnTerrain(EndingTerrain);
 
- Vector3 UpdatePosition(GameObject terrain, Vector3 Position)
- {
-  var t = terrain.GetComponent<Terrain>();
-  return new Vector3(t.Length + Position.x, t.ExitHeight + Position.y);
- }
+    }
+
+    private GameObject SelectTerrain()
+    {
+        var r = Random.Range(0, TerrainBlocks.Length);
+        return TerrainBlocks[r];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void SpawnTerrain(GameObject terrain)
+    {
+        SpawnTerrain(terrain, ActualPosition);
+    }
+
+    void SpawnTerrain(GameObject terrain, Vector3 Position)
+    {
+        Instantiate(terrain, Position, Quaternion.Euler(0, 0, 0));
+        ActualPosition = UpdatePosition(terrain, Position);
+    }
+
+    Vector3 UpdatePosition(GameObject terrain, Vector3 Position)
+    {
+        var t = terrain.GetComponent<Terrain>();
+        return new Vector3(t.Length + Position.x, t.ExitHeight + Position.y);
+    }
 }
