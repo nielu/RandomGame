556adf4c5969b7dabb828fa19547142784a2e82e
Jan Nielek <jan.nielek@intel.com>
2016-12-11 17:26:11 +0100

    add prefabs pack

---
+++ Assets/Scripts/Terrain/MovingTerrain.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingTerrain : Terrain
+{
+
+ public Vector3 From, To;
+ public Vector3 Speed;
+
+ private bool returning = false;
+ private Vector3 localPosition;
+ private GameObject player;
+
+ void Start()
+ {
+  player = GameObject.FindGameObjectWithTag("Player");
+ }
+
+ void Update()
+ {
+  localPosition = transform.localPosition;
+  if (returning)
+  {
+   transform.Translate(-Speed * Time.deltaTime);
+   if (Vector3.Distance(localPosition, From) < 0.05f)
+    returning = false;
+  }
+  else
+  {
+   transform.Translate(Speed * Time.deltaTime);
+   if (Vector3.Distance(localPosition, To) < 0.05f)
+    returning = true;
+  }
+ }
+
+ void OnCollisionEnter2D(Collision2D coll)
+ {
+  if (coll.gameObject.tag == "Player" || coll.gameObject.tag == "enemy")
+  {
+   MakeChild(coll.gameObject);
+  }
+ }
+
+ void OnCollisionExit2D(Collision2D coll)
+ {
+  if (coll.gameObject.tag == "Player" || coll.gameObject.tag == "enemy")
+  {
+   ReleaseChild(coll.gameObject);
+  }
+ }
+
+ private void ReleaseChild(GameObject g)
+ {
+  g.transform.parent = null;
+ }
+
+ private void MakeChild(GameObject g)
+ {
+  g.transform.parent = transform;
+ }
+
+}
---
+++ Assets/Scripts/enemy/blocker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blocker : enemy
+{
+    public float JumpTime;
+
+    private float timer = 0;
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+        JumpTime += Random.Range(-0.2f, 0.2f);
+    }
+
+    // Update is called once per frame
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+
+        timer += Time.deltaTime;
+        if (timer > JumpTime)
+        {
+            body.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
+            timer = 0;
+        }
+    }
+}
