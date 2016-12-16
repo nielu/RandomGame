5cfe040737d79847916397abe607c04868315af2
Jan Nielek <jan.nielek@intel.com>
2016-12-16 15:40:20 +0100

    Introduced random speed change in MovingTerrain (0.8-1.2x)

--- Assets/Scripts/Terrain/MovingTerrain.cs
+++ Assets/Scripts/Terrain/MovingTerrain.cs
@@ -4,59 +4,61 @@ using System.Collections;
 public class MovingTerrain : Terrain
 {
 
- public Vector3 From, To;
- public Vector3 Speed;
+    public Vector3 From, To;
+    public Vector3 Speed;
 
- private bool returning = false;
- private Vector3 localPosition;
- private GameObject player;
+    private bool returning = false;
+    private Vector3 localPosition;
+    private GameObject player;
 
- void Start()
- {
-  player = GameObject.FindGameObjectWithTag("Player");
- }
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
 
- void Update()
- {
-  localPosition = transform.localPosition;
-  if (returning)
-  {
-   transform.Translate(-Speed * Time.deltaTime);
-   if (Vector3.Distance(localPosition, From) < 0.05f)
-    returning = false;
-  }
-  else
-  {
-   transform.Translate(Speed * Time.deltaTime);
-   if (Vector3.Distance(localPosition, To) < 0.05f)
-    returning = true;
-  }
- }
+        Speed *= Random.Range(0.8f, 1.2f);
+    }
 
- void OnCollisionEnter2D(Collision2D coll)
- {
-  if (coll.gameObject.tag == "Player" || coll.gameObject.tag == "enemy")
-  {
-   MakeChild(coll.gameObject);
-  }
- }
+    void Update()
+    {
+        localPosition = transform.localPosition;
+        if (returning)
+        {
+            transform.Translate(-Speed * Time.deltaTime);
+            if (Vector3.Distance(localPosition, From) < 0.05f)
+                returning = false;
+        }
+        else
+        {
+            transform.Translate(Speed * Time.deltaTime);
+            if (Vector3.Distance(localPosition, To) < 0.05f)
+                returning = true;
+        }
+    }
 
- void OnCollisionExit2D(Collision2D coll)
- {
-  if (coll.gameObject.tag == "Player" || coll.gameObject.tag == "enemy")
-  {
-   ReleaseChild(coll.gameObject);
-  }
- }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player" || coll.gameObject.tag == "enemy")
+        {
+            MakeChild(coll.gameObject);
+        }
+    }
 
- private void ReleaseChild(GameObject g)
- {
-  g.transform.parent = null;
- }
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player" || coll.gameObject.tag == "enemy")
+        {
+            ReleaseChild(coll.gameObject);
+        }
+    }
 
- private void MakeChild(GameObject g)
- {
-  g.transform.parent = transform;
- }
+    private void ReleaseChild(GameObject g)
+    {
+        g.transform.parent = null;
+    }
+
+    private void MakeChild(GameObject g)
+    {
+        g.transform.parent = transform;
+    }
 
 }
