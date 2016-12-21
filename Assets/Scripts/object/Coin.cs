73d1151050289800efb7db29eb9c7e2c0252c4cd
Jan Nielek <jan.nielek@intel.com>
2016-12-21 15:15:12 +0100

    Add Coin collectables

---
+++ Assets/Scripts/object/Coin.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int Value;
+
+    private GameObject Player;
+    private Rigidbody2D Body;
+    // Use this for initialization
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Body = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void DestroyMe()
+    {
+        DestroyImmediate(this.gameObject.transform.parent.gameObject);
+    }
+
+    public void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(Player.CompareTag(coll.tag))
+        {
+            Player.GetComponent<PlayerScript>().Score += Value;
+            Body.gravityScale = 1.0f;
+            Body.AddForce(Vector2.up * 5.0f, ForceMode2D.Impulse);
+            Invoke("DestroyMe", 2.0f);
+        }
+    }
+}
